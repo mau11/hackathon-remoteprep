abcc646e06d6a8e9834cf08706faa194bff9fc0f
mau11 <mirvil7130@aol.com>
2016-06-18 13:43:51 -0400

    hackathon project

---
+++ README.md
@@ -0,0 +1,12 @@
+//THURRSDAY, 6/17/16 - SATURDAY, 6/19/16
+//SECTION #2
+//(#5)HALFWAY HACKATHON
+//TEAM- Volkan, Jeff D, Tom & Maureen
+
+FEARSOME FOURSOMES WEBPAGE
+
+This page utilizes on click toggling, randomized background colors and embedded background music. 
+
+In order to change the background color each time the page is refreshed, we used the Math.random() method.
+
+To enable each of the graphics on the page to switch between two images, we used jQuery's on click function. Try clicking on each image to find a hidden surprise!
---
+++ main.js
@@ -0,0 +1,76 @@
+//THURRSDAY, 6/17/16 - SATURDAY, 6/19/16
+//SECTION #2
+//(#5)HALFWAY HACKATHON
+//TEAM- Volkan, Jeff D & Tom
+
+/*Halfway Hackathon
+
+Congratulations! Today we are at the halfway point in our class. Let's celebrate with a hackathon! A hackathon is a great opportunity to work on a larger, real-world project, while reviewing and solidifying the core concepts that we've covered in the first half of the course.
+
+Step 1: Make a Plan
+
+Take a few minutes to brainstorm some project ideas. This could be related to your community site, a stand-alone project, or just a crazy mash-up site to help you practice different coding concepts
+Review the Step 3: Coding section below to consider the different concepts that we hope to practice during this project.
+In groups of four, hold a standup meeting to present your plan of what you'd like to accomplish, and potential stumbling blocks.
+If someone mentions being concerned about a concept that you feel solid on, offer to be a resource for them if they have questions.
+Step 2: Set up a GitHub repo
+
+Open the Terminal and run:
+git config --global user.name "YOUR NAME"
+git config --global user.email "YOUR EMAIL ADDRESS"
+Go to GitHub and sign in or sign up for an account
+Create a new repository on GitHub by clicking the new repository menu, upper-right
+Follow the instructions presented in the new repository
+If you'd like to know more about the philosophy behind version control, check out The Git Parable
+Step 3: Coding
+
+The following lists the core web development skills that you should feel comfortable using in your project thus far:
+
+Use Markdown to create a README.md file introducing your project. Push to GitHub and see it rendered on your repository's main page
+Create an HTML document using the following HTML tags:
+html, head, title, body
+header and paragraph tags
+bold and italic tags
+divs and spans
+ordered and unordered lists
+images and links
+horizontal rule and line break
+Use a style attribute on an HTML element, then a <style> tag in the head, then a <link> tag to a separate .css document to include CSS styles in your page. Try to use the following CSS properties at a minimum:
+color, background-color, background-image
+font-size, font-family
+height, width, max-width
+margin, padding, border, border-radius
+Refactor your code to use classes and ids.
+Use some complex CSS selectors such as descendent selectors
+Use an iframe to embed videos or maps on your page
+Use float and and position: fixed to position some elements
+Create an HTML form incorporating various inputs (text, password, email, textarea, radio, checkbox, select menu, submit, etc)
+Make your form live using Formspree
+Use the Bootstrap grid system to lay out your page
+Use some pre-written Bootstrap CSS classes on various elements of your page
+Use alert, prompt, and console.log where appropriate
+Set and retrieve values from JavaScript variables
+Use document.querySelector in conjunction with textContent and innerHTML to retrieve and place content on the page
+Use if, elseif and else to implement branching logic on the basis of comparing variables and values
+Use logical and (&&), logical or (||), and the ternary operator
+Use while loops to do work multiple times until a condition is met
+Use the Math object and parseInt
+Use jQuery to select existing HTML elements on the page
+Use jQuery's .css(), .attr(), .text(), .html(), .append(), and .val() to both get and set values
+Use some jQuery effects like .show(), .slideDown() and .fadeOut()
+Stretch Goals
+
+Use some of Bootstrap's JavaScript utilties
+Try incorporating some jQuery Plug-ins
+Step 4: Deploy & Promote
+
+Deploy your code to Github Pages using your command line tools
+Post a link on Slack so your classmates can enjoy your work
+Send it to your friends and family members so they can be proud of you
+Social media?
+Step 5: Profit?
+
+Monetize
+Monetize
+Monetize*/
+
